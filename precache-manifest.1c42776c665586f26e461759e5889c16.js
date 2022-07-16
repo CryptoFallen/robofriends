d66c13e5da04514a11cc58f452816b665f6a76b9
CryptoFallen <richardtom1203@protonmail.com>
2022-07-16 11:59:08 +0200

    Updates

---
+++ precache-manifest.1c42776c665586f26e461759e5889c16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fa978bd71b7c59f1890",
+    "url": "/robofriends/static/css/main.dfb1ebe2.chunk.css"
+  },
+  {
+    "revision": "2fa978bd71b7c59f1890",
+    "url": "/robofriends/static/js/main.2fa978bd.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "8c0b72d67611e3f5b010",
+    "url": "/robofriends/static/css/2.8a719eb5.chunk.css"
+  },
+  {
+    "revision": "8c0b72d67611e3f5b010",
+    "url": "/robofriends/static/js/2.8c0b72d6.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b7d4b068e4a9bcffd26d0d2914f4d830",
+    "url": "/robofriends/index.html"
+  }
+];
