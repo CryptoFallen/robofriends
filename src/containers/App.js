605ae9b50f8386cd633370de38d6cb4bdcd8a9c3
CryptoFallen <richardtom1203@protonmail.com>
2022-07-16 10:12:37 +0200

    Tidied code in App.js, created components and containers folders to add files in and help project folder look tidier

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components//Scroll';
 import './App.css';
 
 class App extends Component {
@@ -23,15 +23,16 @@ class App extends Component {
         this.setState({searchfield: event.target.value})
     }
 
-
     render() {
-        const filteredRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        // Destructuring this.state to make code simpler
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) { 
-            return <h1 className='tc'>Loading</h1>
-        } else {
-            return (
+        // replaced === 0 with ! which means not 0
+        return !robots.length ? 
+        <h1 className='tc'>Loading</h1> :
+            (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -40,7 +41,6 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-        }
     }
 }
 
